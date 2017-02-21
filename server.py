84c4569f2e020a1e18f676494c6a11307b12f5a6
Alexey Degtyarev <a.degtyarev@corp.mail.ru>
2017-02-21 14:52:03 +0300

    1

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import mimetypes
-from modules.getfiles import get_dir_files_data
+import os
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -10,8 +10,12 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'application/json')
             self.end_headers()
 
-            message = get_dir_files_data()
-            print(message.replace('\n', '\\n')[:150]+'...')
+            try:
+                from modules.getfiles import get_dir_files_data
+                message = get_dir_files_data()
+                print(message.replace('\n', '\\n')[:150]+'...')
+            except:
+                message =  open('data.cache', 'rt').read()
 
             self.wfile.write(bytes(message, "utf8"))
         else:
@@ -19,11 +23,13 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             if self.path and self.path != '/':
                 file_path = './'+self.path
 
-            mimetype = mimetypes.MimeTypes().guess_type(file_path)[0]
-            self.send_header('Content-type', mimetype)
-            self.end_headers()
-
-            self.wfile.write(open(file_path, 'rb').read())
+            if os.path.exists(file_path):
+                mimetype = mimetypes.MimeTypes().guess_type(file_path)[0]
+                self.send_header('Content-type', mimetype)
+                self.wfile.write(open(file_path, 'rb').read())
+            else:
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
 
 def run():
   print('starting server...')
